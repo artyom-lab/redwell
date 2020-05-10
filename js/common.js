1bc0103a01f5b7fe5014e522be62ba766e36f21e
artyom-lab <artyomgalkeane@gmail.com>
2020-05-10 23:58:39 +0300

    67

--- js/common.js
+++ js/common.js
@@ -15,3 +15,59 @@ $(document).ready(function() {
 	Waves.init();
 
 });
+
+
+var s3Uploader = new qq.FineUploader({
+  // debug: true,
+  element: document.getElementById('fine-uploader-s3'),
+  template: 'qq-template-s3',
+  // request: {
+  //     endpoint: "https://upload.fineuploader.com",
+  //     accessKey: "AKIAJB6BSMFWTAXC5M2Q"
+  // },
+  // signature: {
+  //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php"
+  // },
+  // uploadSuccess: {
+  //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php?success",
+  //     params: {
+  //         isBrowserPreviewCapable: qq.supportedFeatures.imagePreviews
+  //     }
+  // },
+  // iframeSupport: {
+  //     localBlankPagePath: "/server/success.html"
+  // },
+  // cors: {
+  //     expected: true
+  // },
+  // chunking: {
+  //     enabled: true
+  // },
+  // resume: {
+  //     enabled: true
+  // },
+  // deleteFile: {
+  //     enabled: true,
+  //     method: "POST",
+  //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php"
+  // },
+  // validation: {
+  //     itemLimit: 5,
+  //     sizeLimit: 15000000
+  // },
+  // thumbnails: {
+  //     placeholders: {
+  //         notAvailablePath: "libs/fine-uploader/placeholders/not_available-generic.png",
+  //         waitingPath: "libs/fine-uploader/placeholders/waiting-generic.png"
+  //     }
+  // },
+  // callbacks: {
+  //     onComplete: function(id, name, response) {
+  //         var previewLink = qq(this.getItemByFileId(id)).getByClass('preview-link')[0];
+
+  //         if (response.success) {
+  //             previewLink.setAttribute("href", response.tempLink)
+  //         }
+  //     }
+  // }
+});
