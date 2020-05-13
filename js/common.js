d0b497b435d799ad8af3082836d00a9777a08311
artyom-lab <artyomgalkeane@gmail.com>
2020-05-13 21:48:10 +0300

    89

--- js/common.js
+++ js/common.js
@@ -14,107 +14,109 @@ $(document).ready(function() {
 	Waves.attach('.wave2', ['waves-dark']);
 	Waves.init();
 
-});
+  var body = $('body');
 
-$('input[name="birthday"]').daterangepicker({
-  singleDatePicker: true,
-  showDropdowns: true,
-  minYear: 2000,
-  maxYear: parseInt(moment().format('YYYY'),10)
-});
+  function goToNextInput(e) {
+    var key = e.which,
+      t = $(e.target),
+      sib = t.next('input');
 
-var body = $('body');
+    if (key != 9 && (key < 48 || key > 57)) {
+      e.preventDefault();
+      return false;
+    }
 
-function goToNextInput(e) {
-  var key = e.which,
-    t = $(e.target),
-    sib = t.next('input');
+    if (key === 9) {
+      return true;
+    }
 
-  if (key != 9 && (key < 48 || key > 57)) {
+    if (!sib || !sib.length) {
+      sib = body.find('input').eq(0);
+    }
+    sib.select().focus();
+  };
+
+  function onKeyDown(e) {
+    var key = e.which;
+
+    if (key === 9 || (key >= 48 && key <= 57)) {
+      return true;
+    }
+
     e.preventDefault();
     return false;
-  }
+  };
 
-  if (key === 9) {
-    return true;
-  }
+  function onFocus(e) {
+    $(e.target).select();
+  };
 
-  if (!sib || !sib.length) {
-    sib = body.find('input').eq(0);
-  }
-  sib.select().focus();
-}
+  body.on('keyup', 'input', goToNextInput);
+  body.on('keydown', 'input', onKeyDown);
+  body.on('click', 'input', onFocus);
 
-function onKeyDown(e) {
-  var key = e.which;
+  var s3Uploader = new qq.FineUploader({
+    // debug: true,
+    element: document.getElementById('fine-uploader-s3'),
+    template: 'qq-template-s3',
+    // request: {
+    //     endpoint: "https://upload.fineuploader.com",
+    //     accessKey: "AKIAJB6BSMFWTAXC5M2Q"
+    // },
+    // signature: {
+    //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php"
+    // },
+    // uploadSuccess: {
+    //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php?success",
+    //     params: {
+    //         isBrowserPreviewCapable: qq.supportedFeatures.imagePreviews
+    //     }
+    // },
+    // iframeSupport: {
+    //     localBlankPagePath: "/server/success.html"
+    // },
+    // cors: {
+    //     expected: true
+    // },
+    // chunking: {
+    //     enabled: true
+    // },
+    // resume: {
+    //     enabled: true
+    // },
+    // deleteFile: {
+    //     enabled: true,
+    //     method: "POST",
+    //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php"
+    // },
+    // validation: {
+    //     itemLimit: 5,
+    //     sizeLimit: 15000000
+    // },
+    // thumbnails: {
+    //     placeholders: {
+    //         notAvailablePath: "libs/fine-uploader/placeholders/not_available-generic.png",
+    //         waitingPath: "libs/fine-uploader/placeholders/waiting-generic.png"
+    //     }
+    // },
+    // callbacks: {
+    //     onComplete: function(id, name, response) {
+    //         var previewLink = qq(this.getItemByFileId(id)).getByClass('preview-link')[0];
 
-  if (key === 9 || (key >= 48 && key <= 57)) {
-    return true;
-  }
+    //         if (response.success) {
+    //             previewLink.setAttribute("href", response.tempLink)
+    //         }
+    //     }
+    // }
 
-  e.preventDefault();
-  return false;
-}
+  });
 
-function onFocus(e) {
-  $(e.target).select();
-}
+  $('input[name="birthday"]').daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    minYear: 2000,
+    maxYear: parseInt(moment().format('YYYY'),10)
+  });
 
-body.on('keyup', 'input', goToNextInput);
-body.on('keydown', 'input', onKeyDown);
-body.on('click', 'input', onFocus);
-
-var s3Uploader = new qq.FineUploader({
-  // debug: true,
-  element: document.getElementById('fine-uploader-s3'),
-  template: 'qq-template-s3',
-  // request: {
-  //     endpoint: "https://upload.fineuploader.com",
-  //     accessKey: "AKIAJB6BSMFWTAXC5M2Q"
-  // },
-  // signature: {
-  //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php"
-  // },
-  // uploadSuccess: {
-  //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php?success",
-  //     params: {
-  //         isBrowserPreviewCapable: qq.supportedFeatures.imagePreviews
-  //     }
-  // },
-  // iframeSupport: {
-  //     localBlankPagePath: "/server/success.html"
-  // },
-  // cors: {
-  //     expected: true
-  // },
-  // chunking: {
-  //     enabled: true
-  // },
-  // resume: {
-  //     enabled: true
-  // },
-  // deleteFile: {
-  //     enabled: true,
-  //     method: "POST",
-  //     endpoint: "https://s3-demo.fineuploader.com/s3demo-thumbnails-cors.php"
-  // },
-  // validation: {
-  //     itemLimit: 5,
-  //     sizeLimit: 15000000
-  // },
-  // thumbnails: {
-  //     placeholders: {
-  //         notAvailablePath: "libs/fine-uploader/placeholders/not_available-generic.png",
-  //         waitingPath: "libs/fine-uploader/placeholders/waiting-generic.png"
-  //     }
-  // },
-  // callbacks: {
-  //     onComplete: function(id, name, response) {
-  //         var previewLink = qq(this.getItemByFileId(id)).getByClass('preview-link')[0];
-
-  //         if (response.success) {
-  //             previewLink.setAttribute("href", response.tempLink)
-  //         }
-  //     }
-  // }
 });
+
