ea3ebff6a3b4793105a5592f18913930d3b5ce82
artyom-lab <artyomgalkeane@gmail.com>
2020-05-13 21:42:16 +0300

    56

--- js/common.js
+++ js/common.js
@@ -16,6 +16,53 @@ $(document).ready(function() {
 
 });
 
+$('input[name="birthday"]').daterangepicker({
+  singleDatePicker: true,
+  showDropdowns: true,
+  minYear: 2000,
+  maxYear: parseInt(moment().format('YYYY'),10)
+});
+
+var body = $('body');
+
+function goToNextInput(e) {
+  var key = e.which,
+    t = $(e.target),
+    sib = t.next('input');
+
+  if (key != 9 && (key < 48 || key > 57)) {
+    e.preventDefault();
+    return false;
+  }
+
+  if (key === 9) {
+    return true;
+  }
+
+  if (!sib || !sib.length) {
+    sib = body.find('input').eq(0);
+  }
+  sib.select().focus();
+}
+
+function onKeyDown(e) {
+  var key = e.which;
+
+  if (key === 9 || (key >= 48 && key <= 57)) {
+    return true;
+  }
+
+  e.preventDefault();
+  return false;
+}
+
+function onFocus(e) {
+  $(e.target).select();
+}
+
+body.on('keyup', 'input', goToNextInput);
+body.on('keydown', 'input', onKeyDown);
+body.on('click', 'input', onFocus);
 
 var s3Uploader = new qq.FineUploader({
   // debug: true,
